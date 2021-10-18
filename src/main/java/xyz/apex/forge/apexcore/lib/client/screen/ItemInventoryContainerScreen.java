3dbf2891a8ebf8d904e15ead61821b1b9f5b071b
ApexModder <robsonlawson3@gmail.com>
2021-10-18 05:50:57 +0100

    Add ItemInventoryContainerScreen
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

---
+++ src/main/java/xyz/apex/forge/apexcore/lib/client/screen/ItemInventoryContainerScreen.java
@@ -0,0 +1,49 @@
+package xyz.apex.forge.apexcore.lib.client.screen;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import xyz.apex.forge.apexcore.lib.container.ItemInventoryContainer;
+
+@OnlyIn(Dist.CLIENT)
+public class ItemInventoryContainerScreen<C extends ItemInventoryContainer> extends ContainerScreen<C>
+{
+	protected final ResourceLocation backgroundTexture;
+
+	public ItemInventoryContainerScreen(C container, PlayerInventory playerInventory, ITextComponent title, ResourceLocation backgroundTexture)
+	{
+		super(container, playerInventory, title);
+
+		this.backgroundTexture = backgroundTexture;
+	}
+
+	@Override
+	protected void init()
+	{
+		super.init();
+		titleLabelX = (imageWidth - font.width(title)) / 2;
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks)
+	{
+		renderBackground(matrixStack);
+		super.render(matrixStack, mouseX, mouseY, partialTicks);
+		renderTooltip(matrixStack, mouseX, mouseY);
+	}
+
+	@Override
+	protected void renderBg(MatrixStack matrixStack, float partialTicks, int mouseX, int mouseY)
+	{
+		RenderSystem.color4f(1F, 1F, 1F, 1F);
+		minecraft.getTextureManager().bind(backgroundTexture);
+		int i = (width - imageWidth) / 2;
+		int j = (height - imageHeight) / 2;
+		blit(matrixStack, i, j, 0, 0, imageWidth, imageHeight);
+	}
+}
---
+++ src/main/java/xyz/apex/forge/apexcore/lib/client/screen/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault @ParametersAreNonnullByDefault @FieldsAreNonnullByDefault
+package xyz.apex.forge.apexcore.lib.client.screen;
+
+import com.tterrag.registrate.util.nullness.FieldsAreNonnullByDefault;
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
