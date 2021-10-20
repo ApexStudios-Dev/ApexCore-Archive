9c8ecd6c559f7507b61def06242aa9716db025cf
ApexModder <robsonlawson3@gmail.com>
2021-10-20 03:02:06 +0100

    Add wrapper functional interface to make vanillas single item recipe factory interface public
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

--- src/main/java/xyz/apex/forge/apexcore/lib/item/crafting/SingleItemRecipeSerializer.java
+++ src/main/java/xyz/apex/forge/apexcore/lib/item/crafting/SingleItemRecipeSerializer.java
@@ -1,11 +1,19 @@
 package xyz.apex.forge.apexcore.lib.item.crafting;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.crafting.SingleItemRecipe;
+import net.minecraft.util.ResourceLocation;
 
 public class SingleItemRecipeSerializer<T extends SingleItemRecipe> extends SingleItemRecipe.Serializer<T>
 {
 	public SingleItemRecipeSerializer(IRecipeFactory<T> recipeFactory)
 	{
 		super(recipeFactory);
+	}
+
+	public interface IRecipeFactory<T extends SingleItemRecipe> extends SingleItemRecipe.Serializer.IRecipeFactory<T>
+	{
+		T create(ResourceLocation recipeName, String group, Ingredient ingredient, ItemStack stack);
 	}
 }
