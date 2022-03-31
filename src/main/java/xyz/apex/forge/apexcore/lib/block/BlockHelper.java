6f48c95c1e66f1fdb70386d9d42d28c6f4eb7aed
ApexModder <robsonlawson3@gmail.com>
2022-03-31 05:04:06 +0100

    Add various new helper methods to BlockHelper
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

--- src/main/java/xyz/apex/forge/apexcore/lib/block/BlockHelper.java
+++ src/main/java/xyz/apex/forge/apexcore/lib/block/BlockHelper.java
@@ -1,11 +1,17 @@
 package xyz.apex.forge.apexcore.lib.block;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.state.Property;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
 import java.util.function.ToIntFunction;
 
 public final class BlockHelper
@@ -38,5 +44,57 @@ public final class BlockHelper
 	public static boolean never(BlockState blockState, IBlockReader level, BlockPos pos)
 	{
 		return false;
+	}
+
+	public static void playBreakSound(World level, BlockPos pos, @Nullable Entity entity)
+	{
+		BlockState blockState = level.getBlockState(pos);
+		SoundType soundType = blockState.getSoundType(level, pos, entity);
+		level.playSound(null, pos, soundType.getBreakSound(), SoundCategory.BLOCKS, (soundType.getVolume() + 1F) / 2F, soundType.getPitch() * .8F);
+	}
+
+	public static void playPlaceSound(World level, BlockPos pos, @Nullable Entity entity)
+	{
+		BlockState blockState = level.getBlockState(pos);
+		SoundType soundType = blockState.getSoundType(level, pos, entity);
+		level.playSound(null, pos, soundType.getPlaceSound(), SoundCategory.BLOCKS, (soundType.getVolume() + 1F) / 2F, soundType.getPitch() * .8F);
+	}
+
+	public static void playStepSound(World level, BlockPos pos, @Nullable Entity entity)
+	{
+		BlockState blockState = level.getBlockState(pos);
+		SoundType soundType = blockState.getSoundType(level, pos, entity);
+		level.playSound(null, pos, soundType.getStepSound(), SoundCategory.BLOCKS, (soundType.getVolume() + 1F) / 2F, soundType.getPitch() * .8F);
+	}
+
+	public static void playFallSound(World level, BlockPos pos, @Nullable Entity entity)
+	{
+		BlockState blockState = level.getBlockState(pos);
+		SoundType soundType = blockState.getSoundType(level, pos, entity);
+		level.playSound(null, pos, soundType.getFallSound(), SoundCategory.BLOCKS, (soundType.getVolume() + 1F) / 2F, soundType.getPitch() * .8F);
+	}
+
+	public static void playHitSound(World level, BlockPos pos, @Nullable Entity entity)
+	{
+		BlockState blockState = level.getBlockState(pos);
+		SoundType soundType = blockState.getSoundType(level, pos, entity);
+		level.playSound(null, pos, soundType.getHitSound(), SoundCategory.BLOCKS, (soundType.getVolume() + 1F) / 2F, soundType.getPitch() * .8F);
+	}
+
+	public static BlockState copyBlockProperties(BlockState from, BlockState to)
+	{
+		for(Property<?> property : from.getProperties())
+		{
+			to = copyBlockProperty(from, to, property);
+		}
+
+		return to;
+	}
+
+	public static <T extends Comparable<T>> BlockState copyBlockProperty(BlockState from, BlockState to, Property<T> property)
+	{
+		if(from.hasProperty(property) && to.hasProperty(property))
+			return to.setValue(property, from.getValue(property));
+		return to;
 	}
 }
