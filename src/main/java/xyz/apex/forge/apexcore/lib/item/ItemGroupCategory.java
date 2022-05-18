754dca702709b9f438f5e5bfbf75e2bdb0650af7
ApexModder <robsonlawson3@gmail.com>
2022-05-18 22:45:13 +0100

    Allow ItemGroup Categories to have a static none cycling icon Add method to register translation keys for ItemGroup Categories
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

--- src/main/java/xyz/apex/forge/apexcore/lib/item/ItemGroupCategory.java
+++ src/main/java/xyz/apex/forge/apexcore/lib/item/ItemGroupCategory.java
@@ -11,10 +11,17 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 
+import xyz.apex.forge.utility.registrator.AbstractRegistrator;
+import xyz.apex.java.utility.Lazy;
 import xyz.apex.java.utility.nullness.NonnullPredicate;
+import xyz.apex.java.utility.nullness.NonnullSupplier;
 
+import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Map;
+
+import static xyz.apex.forge.utility.registrator.AbstractRegistrator.LANG_EXT_PROVIDER;
+import static com.tterrag.registrate.providers.ProviderType.LANG;
 
 public final class ItemGroupCategory implements NonnullPredicate<ItemStack>
 {
@@ -25,11 +32,15 @@ public final class ItemGroupCategory implements NonnullPredicate<ItemStack>
 	private long cycleStartTime;
 	private long cycleTickTime;
 	private long cyclePausedDuration = 0L;
+	private final boolean cycleIcons;
+	private final Lazy<ItemStack> defaultIcon;
 
 	private ItemGroupCategory(Builder builder)
 	{
 		this.builder = builder;
 
+		defaultIcon = Lazy.of(() -> builder.defaultIcon == null ? Items.BARRIER.getDefaultInstance() : builder.defaultIcon.get());
+		cycleIcons = builder.cycleIcons;
 		cycleTickTime = System.currentTimeMillis();
 		cycleStartTime = cycleTickTime - ((long) builder.cycleOffset * cycleTime);
 	}
@@ -46,6 +57,21 @@ public final class ItemGroupCategory implements NonnullPredicate<ItemStack>
 				icons.add(stack);
 		}
 
+		ItemStack defaultIcon = this.defaultIcon.get();
+
+		if(icons.isEmpty())
+			icons.add(defaultIcon);
+		else
+		{
+			for(ItemStack icon : icons)
+			{
+				if(ItemStack.isSame(icon, defaultIcon))
+					return icons;
+			}
+
+			icons.add(defaultIcon);
+		}
+
 		return icons;
 	}
 
@@ -55,31 +81,44 @@ public final class ItemGroupCategory implements NonnullPredicate<ItemStack>
 		return builder.categoryPredicate.test(stack);
 	}
 
-	public void tick(boolean cycleIcons)
+	public boolean isCyclingIcons()
+	{
+		return cycleIcons;
+	}
+
+	public void tick(boolean doCycle)
 	{
 		if(cycleIcons)
 		{
-			if(cyclePausedDuration > 0L)
+			if(cycleIcons)
 			{
-				cycleStartTime += cyclePausedDuration;
-				cyclePausedDuration = 0L;
-			}
+				if(cyclePausedDuration > 0L)
+				{
+					cycleStartTime += cyclePausedDuration;
+					cyclePausedDuration = 0L;
+				}
 
-			cycleTickTime = System.currentTimeMillis();
+				cycleTickTime = System.currentTimeMillis();
+			}
+			else
+				cyclePausedDuration = System.currentTimeMillis() - cycleTickTime;
 		}
-		else
-			cyclePausedDuration = System.currentTimeMillis() - cycleTickTime;
 	}
 
 	public ItemStack getCategoryIcon(ItemGroup itemGroup)
 	{
-		List<ItemStack> icons = categoryIcons.computeIfAbsent(itemGroup, this::getCategoryIcons);
+		if(cycleIcons)
+		{
+			List<ItemStack> icons = categoryIcons.computeIfAbsent(itemGroup, this::getCategoryIcons);
 
-		if(icons.isEmpty())
-			return Items.BARRIER.getDefaultInstance();
+			if(icons.isEmpty())
+				return defaultIcon.get();
 
-		long index = ((cycleTickTime - cycleStartTime) / cycleTime) % icons.size();
-		return icons.get(Math.toIntExact(index));
+			long index = ((cycleTickTime - cycleStartTime) / cycleTime) % icons.size();
+			return icons.get(Math.toIntExact(index));
+		}
+		else
+			return defaultIcon.get();
 	}
 
 	public String getCategoryNameKey()
@@ -93,6 +132,18 @@ public final class ItemGroupCategory implements NonnullPredicate<ItemStack>
 		return new TranslationTextComponent(categoryNameKey);
 	}
 
+	public <T extends AbstractRegistrator<T>> ItemGroupCategory addTranslationGenerator(T registrator, String englishName)
+	{
+		registrator.addDataGenerator(LANG, provider -> provider.add(getCategoryNameKey(), englishName));
+		return this;
+	}
+
+	public <T extends AbstractRegistrator<T>> ItemGroupCategory addTranslationGenerator(T registrator, String languageKey, String translationValue)
+	{
+		registrator.addDataGenerator(LANG_EXT_PROVIDER, provider -> provider.add(languageKey, getCategoryNameKey(), translationValue));
+		return this;
+	}
+
 	public static Builder builder(String categoryName)
 	{
 		return new Builder(categoryName);
@@ -102,7 +153,9 @@ public final class ItemGroupCategory implements NonnullPredicate<ItemStack>
 	{
 		private final String categoryName;
 		private int cycleOffset = (int) ((Math.random() * 10000) % Integer.MAX_VALUE);
+		private boolean cycleIcons = false;
 		private NonnullPredicate<ItemStack> categoryPredicate = stack -> true;
+		@Nullable private NonnullSupplier<ItemStack> defaultIcon;
 
 		private Builder(String categoryName)
 		{
@@ -115,6 +168,12 @@ public final class ItemGroupCategory implements NonnullPredicate<ItemStack>
 			return this;
 		}
 
+		public Builder defaultIcon(NonnullSupplier<ItemStack> defaultIcon)
+		{
+			this.defaultIcon = defaultIcon;
+			return this;
+		}
+
 		public Builder tagged(ITag<Item> itemTag)
 		{
 			return predicate(s -> s.getItem().is(itemTag));
@@ -122,6 +181,7 @@ public final class ItemGroupCategory implements NonnullPredicate<ItemStack>
 
 		public Builder cycleOffset(int cycleOffset)
 		{
+			cycleIcons = true;
 			this.cycleOffset = cycleOffset;
 			return this;
 		}
