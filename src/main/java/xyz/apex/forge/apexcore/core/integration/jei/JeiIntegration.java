88390b1da58056adabc69674cec1d8fc80c8e2fc
ApexModder <robsonlawson3@gmail.com>
2022-05-18 08:10:19 +0100

    Add Jei Integration
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

---
+++ src/main/java/xyz/apex/forge/apexcore/core/integration/jei/JeiIntegration.java
@@ -0,0 +1,29 @@
+package xyz.apex.forge.apexcore.core.integration.jei;
+
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.ISubtypeRegistration;
+
+import net.minecraft.util.ResourceLocation;
+
+import xyz.apex.forge.apexcore.core.init.ACRegistry;
+import xyz.apex.forge.apexcore.core.init.PlayerPlushie;
+
+@JeiPlugin
+public final class JeiIntegration implements IModPlugin
+{
+	private final ACRegistry registry = ACRegistry.getRegistry();
+	private final ResourceLocation pluginName = registry.id("jei_plugin");
+
+	@Override
+	public ResourceLocation getPluginUid()
+	{
+		return pluginName;
+	}
+
+	@Override
+	public void registerItemSubtypes(ISubtypeRegistration registration)
+	{
+		registration.useNbtForSubtypes(PlayerPlushie.PLAYER_PLUSHIE_BLOCK_ITEM.asItem());
+	}
+}
