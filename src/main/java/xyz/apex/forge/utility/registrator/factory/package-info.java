39dfe4d4a57d90c006c94b701efd02ad6d963029
Robson Lawson <29412632+ApexModder@users.noreply.github.com>
2022-06-01 01:57:17 +0100

    Delete src/main/java/xyz/apex/forge/utility directory

--- src/main/java/xyz/apex/forge/utility/registrator/factory/package-info.java
+++
@@ -1,6 +0,0 @@
-@MethodsReturnNonnullByDefault @ParametersAreNonnullByDefault @FieldsAreNonnullByDefault
-package xyz.apex.forge.utility.registrator.factory;
-
-import xyz.apex.java.utility.nullness.FieldsAreNonnullByDefault;
-import xyz.apex.java.utility.nullness.MethodsReturnNonnullByDefault;
-import xyz.apex.java.utility.nullness.ParametersAreNonnullByDefault;
