bb2051292056e17d801277d9c84909f2fbc1b2a4
ApexModder <robsonlawson3@gmail.com>
2022-06-02 21:26:28 +0100

    Fix using incorrect component type for custom block names
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

--- src/main/java/xyz/apex/forge/apexcore/lib/util/NameableMutable.java
+++ src/main/java/xyz/apex/forge/apexcore/lib/util/NameableMutable.java
@@ -1,11 +1,11 @@
 package xyz.apex.forge.apexcore.lib.util;
 
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.Nameable;
 
 import javax.annotation.Nullable;
 
 public interface NameableMutable extends Nameable
 {
-	void setCustomName(@Nullable TextComponent customName);
+	void setCustomName(@Nullable Component customName);
 }
