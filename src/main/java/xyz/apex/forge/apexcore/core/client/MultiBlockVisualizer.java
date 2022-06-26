f10c1338126e642b499bfbecd64c46f282e48d81
ApexModder <robsonlawson3@gmail.com>
2022-06-26 02:55:46 +0100

    Make visualizer outline render debug toggalable
    Signed-off-by: ApexModder <robsonlawson3@gmail.com>

--- src/main/java/xyz/apex/forge/apexcore/core/client/MultiBlockVisualizer.java
+++ src/main/java/xyz/apex/forge/apexcore/core/client/MultiBlockVisualizer.java
@@ -40,6 +40,7 @@ import java.util.Map;
 public final class MultiBlockVisualizer
 {
 	@Nullable private static MultiBufferSource.BufferSource ghostBuffers = null;
+	private static boolean renderOutline =  false;
 
 	@SubscribeEvent
 	public static void onRenderWorld(RenderLevelLastEvent event)
@@ -92,36 +93,37 @@ public final class MultiBlockVisualizer
 
 				placeState = BaseMultiBlock.setWaterLogged(placeState, false);
 
-				// if(placeState.canSurvive(level, renderPos))
+				var position = mc.getEntityRenderDispatcher().camera.getPosition();
+
+				var buffers = mc.renderBuffers().bufferSource();
+
+				var type = RenderType.lines();
+
+				if(ghostBuffers == null)
+					ghostBuffers = initBuffers(buffers);
+
+				pose.pushPose();
+				pose.translate(-position.x, -position.y, -position.z);
+
+				if(renderOutline)
 				{
-					var position = mc.getEntityRenderDispatcher().camera.getPosition();
-
-					pose.pushPose();
-					pose.translate(-position.x, -position.y, -position.z);
-
-					var buffers = mc.renderBuffers().bufferSource();
-
-					var type = RenderType.lines();
-
-					if(ghostBuffers == null)
-						ghostBuffers = initBuffers(buffers);
-
 					var consumer = buffers.getBuffer(type);
-
-					renderBlock(mc, pose, ghostBuffers, consumer, level, renderPos, placeState, multiBlock);
-
+					renderBlockWithOutline(mc, pose, ghostBuffers, consumer, level, renderPos, placeState, multiBlock);
 					buffers.endBatch(type);
-					ghostBuffers.endBatch();
-					pose.popPose();
-					return true;
 				}
+				else
+					renderBlockWithoutOutline(mc, pose, ghostBuffers, level, renderPos, placeState, multiBlock);
+
+				ghostBuffers.endBatch();
+				pose.popPose();
+				return true;
 			}
 		}
 
 		return false;
 	}
 
-	private static void renderBlock(Minecraft mc, PoseStack pose, MultiBufferSource.BufferSource bufferSource, VertexConsumer vertexConsumer, ClientLevel level, BlockPos pos, BlockState blockState, IMultiBlock multiBlock)
+	private static void renderBlockWithOutline(Minecraft mc, PoseStack pose, MultiBufferSource.BufferSource bufferSource, VertexConsumer vertexConsumer, ClientLevel level, BlockPos pos, BlockState blockState, IMultiBlock multiBlock)
 	{
 		var origin = multiBlock.getMultiBlockOriginPos(blockState, pos);
 		var localPositions = multiBlock.getMultiBlockLocalPositions();
@@ -149,24 +151,73 @@ public final class MultiBlockVisualizer
 			var localPos = localPositions.get(i);
 			var worldPos = multiBlock.getMultiBlockWorldSpaceFromLocalSpace(blockState, origin, localPos);
 			var renderState = multiBlock.setMultiBlockIndex(blockState, i);
-			renderBlockState(mc, pose, bufferSource, vertexConsumer, level, overlay, worldPos, renderState, multiBlock);
+			renderBlockStateWithOutline(mc, pose, bufferSource, vertexConsumer, level, overlay, worldPos, renderState, multiBlock);
 		}
 	}
 
-	private static void renderBlockState(Minecraft mc, PoseStack pose, MultiBufferSource.BufferSource bufferSource, VertexConsumer vertexConsumer, ClientLevel level, int overlay, BlockPos pos, BlockState blockState, IMultiBlock multiBlock)
+	private static void renderBlockWithoutOutline(Minecraft mc, PoseStack pose, MultiBufferSource.BufferSource bufferSource, ClientLevel level, BlockPos pos, BlockState blockState, IMultiBlock multiBlock)
+	{
+		var origin = multiBlock.getMultiBlockOriginPos(blockState, pos);
+		var localPositions = multiBlock.getMultiBlockLocalPositions();
+
+		var isValid = true;
+
+		for(var i = 0; i < localPositions.size(); i++)
+		{
+			var localPos = localPositions.get(i);
+			var worldPos = multiBlock.getMultiBlockWorldSpaceFromLocalSpace(blockState, origin, localPos);
+			var renderState = multiBlock.setMultiBlockIndex(blockState, i);
+			var worldState = level.getBlockState(worldPos);
+
+			if(!multiBlock.getMultiBlockPattern().passesPlacementTests(multiBlock, level, worldPos, renderState, worldState))
+			{
+				isValid = false;
+				break;
+			}
+		}
+
+		var overlay = isValid ? OverlayTexture.NO_OVERLAY : OverlayTexture.pack(OverlayTexture.NO_WHITE_U, OverlayTexture.RED_OVERLAY_V);
+
+		for(var i = 0; i < localPositions.size(); i++)
+		{
+			var localPos = localPositions.get(i);
+			var worldPos = multiBlock.getMultiBlockWorldSpaceFromLocalSpace(blockState, origin, localPos);
+			var renderState = multiBlock.setMultiBlockIndex(blockState, i);
+			renderBlockStateWithoutOutline(mc, pose, bufferSource, overlay, worldPos, renderState);
+		}
+	}
+
+	private static void renderBlockStateWithOutline(Minecraft mc, PoseStack pose, MultiBufferSource.BufferSource bufferSource, VertexConsumer vertexConsumer, ClientLevel level, int overlay, BlockPos pos, BlockState blockState, IMultiBlock multiBlock)
 	{
 		var x = pos.getX();
 		var y = pos.getY();
 		var z = pos.getZ();
 
-		//if(multiBlock.isMultiBlockOrigin(blockState))
+		if(multiBlock.isMultiBlockOrigin(blockState))
 		{
-			RenderSystem.runAsFancy(() -> {
-				pose.pushPose();
-				renderHitOutline(pose, vertexConsumer, level, pos, blockState, 0F, 0F, 0F, .4F);
-				pose.popPose();
-			});
+			// this does not render during Fabulous graphics due to Forge Issue #8603 (https://github.com/MinecraftForge/MinecraftForge/pull/8603)
+			pose.pushPose();
+			renderHitOutline(pose, vertexConsumer, level, pos, blockState, 0F, 0F, 0F, .4F);
+			pose.popPose();
 		}
+
+		pose.translate(x, y, z);
+
+		if(blockState.getRenderShape() == RenderShape.MODEL)
+		{
+			pose.pushPose();
+			mc.getBlockRenderer().renderSingleBlock(blockState, pose, bufferSource, LightTexture.FULL_BLOCK, overlay, EmptyModelData.INSTANCE);
+			pose.popPose();
+		}
+
+		pose.translate(-x, -y, -z);
+	}
+
+	private static void renderBlockStateWithoutOutline(Minecraft mc, PoseStack pose, MultiBufferSource.BufferSource bufferSource, int overlay, BlockPos pos, BlockState blockState)
+	{
+		var x = pos.getX();
+		var y = pos.getY();
+		var z = pos.getZ();
 
 		pose.translate(x, y, z);
 
