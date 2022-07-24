cd5c78e43b78f02741bc05b6acc7ce78c320358c
ApexModder <robsonlawson3@gmail.com>
2022-07-24 03:00:03 +0100

    Remove JavaUtilities dependency Add functional interfaces from JavaUtilities

---
+++ src/main/java/xyz/apex/forge/apexcore/lib/util/function/TriConsumer.java
@@ -0,0 +1,24 @@
+package xyz.apex.forge.apexcore.lib.util.function;
+
+import java.util.Objects;
+
+@FunctionalInterface
+public interface TriConsumer<A, B, C>
+{
+	void accept(A a, B b, C c);
+
+	default TriConsumer<A, B, C> andThen(TriConsumer<? super A, ? super B, ? super C> after)
+	{
+		Objects.requireNonNull(after);
+
+		return (a, b, c) -> {
+			accept(a, b, c);
+			after.accept(a, b, c);
+		};
+	}
+
+	static <A, B, C> TriConsumer<A, B, C> noop()
+	{
+		return (a, b, c) -> { };
+	}
+}
