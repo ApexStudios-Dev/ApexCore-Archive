26334ca725bf3b739fd271be68bccad34a5f6b64
ApexModder <robsonlawson3@gmail.com>
2022-08-11 06:19:09 +0100

    Update BlockVisualizer to make use of Forges new RenderLevelStage event Abstract out BlockVisualizer to work on any block type not just IMultiBlock types Add new event fired on the Forge EventBus used to modify visualizer rendering and allow custom renders pre/post the rendering

---
+++ src/main/java/xyz/apex/forge/apexcore/lib/event/client/BlockVisualizerEvent.java
@@ -0,0 +1,85 @@
+package xyz.apex.forge.apexcore.lib.event.client;
+
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.eventbus.api.Event;
+
+import xyz.apex.forge.apexcore.core.client.BlockVisualizer;
+
+public class BlockVisualizerEvent extends Event
+{
+	private final BlockVisualizer.Context context;
+
+	protected BlockVisualizerEvent(BlockVisualizer.Context context)
+	{
+		this.context = context;
+	}
+
+	public final BlockVisualizer.Context getContext()
+	{
+		return context;
+	}
+
+	public static class Render extends BlockVisualizerEvent
+	{
+		public final Stage stage;
+
+		protected Render(BlockVisualizer.Context context, Stage stage)
+		{
+			super(context);
+
+			this.stage = stage;
+		}
+
+		public enum Stage
+		{
+			PRE,
+			POST
+		}
+
+		public static final class Pre extends Render
+		{
+			public Pre(BlockVisualizer.Context context)
+			{
+				super(context, Stage.PRE);
+			}
+		}
+
+		public static final class Post extends Render
+		{
+			public Post(BlockVisualizer.Context context)
+			{
+				super(context, Stage.POST);
+			}
+		}
+	}
+
+	public static class ModifyBlockState extends BlockVisualizerEvent
+	{
+		public final Reason reason;
+		private BlockState blockState;
+
+		public ModifyBlockState(BlockVisualizer.Context context, Reason reason)
+		{
+			super(context);
+
+			this.reason = reason;
+			blockState = context.blockState();
+		}
+
+		public BlockState getBlockState()
+		{
+			return blockState;
+		}
+
+		public void setBlockState(BlockState blockState)
+		{
+			this.blockState = blockState;
+		}
+
+		public enum Reason
+		{
+			DEFAULT_BLOCKSTATE,
+			EXISTING_BLOCKSTATE
+		}
+	}
+}
