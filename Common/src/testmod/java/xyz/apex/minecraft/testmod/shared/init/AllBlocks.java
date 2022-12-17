67b4786f599af5cfa85db0d118c8158133d09d43
Apex <robsonlawson3@gmail.com>
2022-12-17 23:42:13 +0000

    Uncomment TestMod Items/Blocks

--- Common/src/testmod/java/xyz/apex/minecraft/testmod/shared/init/AllBlocks.java
+++ Common/src/testmod/java/xyz/apex/minecraft/testmod/shared/init/AllBlocks.java
@@ -1,28 +1,64 @@
 package xyz.apex.minecraft.testmod.shared.init;
 
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+
+import xyz.apex.minecraft.apexcore.shared.registry.BlockBuilder;
+import xyz.apex.minecraft.apexcore.shared.registry.entry.BlockEntry;
+import xyz.apex.minecraft.testmod.shared.TestMod;
+
 public interface AllBlocks
 {
-    /*BlockRegistryEntry<DropExperienceBlock> LEAD_ORE = TestMod.Registries.BLOCKS
-            .builder("lead_ore", DropExperienceBlock::new)
-            .initialProperties(Properties.BLOCK_ORE)
+    BlockEntry<DropExperienceBlock> LEAD_ORE = builder("lead_ore", DropExperienceBlock::new)
+            .copyPropertiesFrom(() -> Blocks.IRON_ORE)
+            .simpleItem()
+//                .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
+            .build()
     .register();
 
-    BlockRegistryEntry<DropExperienceBlock> DEEPSLATE_LEAD_ORE = TestMod.Registries.BLOCKS
-            .builder("deepslate_lead_ore", DropExperienceBlock::new)
-            .initialProperties(Properties.BLOCK_DEEPSLATE_ORE)
+    BlockEntry<DropExperienceBlock> DEEPSLATE_LEAD_ORE = builder("deepslate_lead_ore", DropExperienceBlock::new)
+            .copyPropertiesFrom(() -> Blocks.DEEPSLATE_IRON_ORE)
+            .simpleItem()
+//                .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
+            .build()
     .register();
 
-    BlockRegistryEntry<Block> LEAD_BLOCK = TestMod.Registries.BLOCKS
-            .generic("lead_block")
-            .initialProperties(Properties.BLOCK_STORAGE)
+    BlockEntry<Block> LEAD_BLOCK = builder("lead_block")
+            .copyPropertiesFrom(() -> Blocks.IRON_BLOCK)
+            .simpleItem()
+//                .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
+            .build()
     .register();
 
-    BlockRegistryEntry<Block> RAW_LEAD_BLOCK = TestMod.Registries.BLOCKS
-            .generic("raw_lead_block")
-            .initialProperties(Properties.BLOCK_RAW_ORE)
-    .register();*/
+    BlockEntry<Block> RAW_LEAD_BLOCK = builder("raw_lead_block")
+            .copyPropertiesFrom(() -> Blocks.RAW_IRON_BLOCK)
+            .simpleItem()
+//                .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
+            .build()
+    .register();
 
     static void bootstrap()
     {
+    }
+
+    private static <T extends Block, P> BlockBuilder<T, P> builder(String name, P parent, BlockBuilder.BlockFactory<T> itemFactory)
+    {
+        return BlockBuilder.builder(TestMod.ID, name, parent, itemFactory);
+    }
+
+    private static <T extends Block> BlockBuilder<T, Object> builder(String name, BlockBuilder.BlockFactory<T> itemFactory)
+    {
+        return BlockBuilder.builder(TestMod.ID, name, itemFactory);
+    }
+
+    private static BlockBuilder<Block, Object> builder(String name)
+    {
+        return BlockBuilder.block(TestMod.ID, name);
+    }
+
+    private static <P> BlockBuilder<Block, P> builder(String name, P parent)
+    {
+        return BlockBuilder.block(TestMod.ID, name, parent);
     }
 }
--- Common/src/testmod/java/xyz/apex/minecraft/testmod/shared/init/AllItems.java
+++ Common/src/testmod/java/xyz/apex/minecraft/testmod/shared/init/AllItems.java
@@ -8,116 +8,98 @@ import xyz.apex.minecraft.testmod.shared.TestMod;
 
 public interface AllItems
 {
-    ItemEntry<Item> LEAD_INGOT = ItemBuilder
-            .item(TestMod.ID, "lead_ingot")
+    // EnchancedTier LEAD_ITEM_TIER = TierBuilder.builder().level(2).uses(250).speed(6F).attackDamageBonus(2F).enchantmentValue(14).repairItem(AllItems.LEAD_INGOT).toolLevelTag(BlockTags.NEEDS_IRON_TOOL).build();
+    // ArmorMaterial LEAD_ARMOR_MATERIAL = ArmorMaterialBuilder.builder(TestMod.ID, "lead").durabilityFromModifier(15).defenseFromSlotProtections(2, 5, 6, 2).enchantmentValue(9).toughness(0F).knockbackResistance(0F).repairItem(AllItems.LEAD_INGOT).build();
+
+    /*ItemEntry<SwordItem> LEAD_SWORD = builder("lead_sword")
+            .sword(LEAD_ITEM_TIER)
+            .initialProperties(Properties.ITEM_COMBAT)
+    .register();*/
+
+    /*ItemEntry<PickaxeItem> LEAD_PICKAXE = builder("lead_pickaxe")
+            .pickaxeItem(LEAD_ITEM_TIER)
+            .initialProperties(Properties.ITEM_TOOL)
+    .register();*/
+
+    /*ItemEntry<AxeItem> LEAD_AXE = builder("lead_axe")
+            .axeItem(LEAD_ITEM_TIER)
+            .initialProperties(Properties.ITEM_TOOL)
+    .register();*/
+
+    /*ItemEntry<ShovelItem> LEAD_SHOVEL = builder("lead_shovel")
+            .shovelItem(LEAD_ITEM_TIER)
+            .initialProperties(Properties.ITEM_TOOL)
+    .register();*/
+
+    /*ItemEntry<HoeItem> LEAD_HOE = builder("lead_hoe")
+            .hoeItem(LEAD_ITEM_TIER)
+            .initialProperties(Properties.ITEM_TOOL)
+    .register();*/
+
+    /*ItemEntry<ArmorItem> LEAD_HELMET = builder("lead_helmet")
+            .helmetItem(LEAD_ARMOR_MATERIAL)
+            .initialProperties(Properties.ITEM_COMBAT)
+    .register();*/
+
+    /*ItemEntry<ArmorItem> LEAD_CHESTPLATE = builder("lead_chestplate")
+            .chestplateItem(LEAD_ARMOR_MATERIAL)
+            .initialProperties(Properties.ITEM_COMBAT)
+    .register();*/
+
+    /*ItemEntry<ArmorItem> LEAD_LEGGINGS = builder("lead_leggings")
+            .leggingItem(LEAD_ARMOR_MATERIAL)
+            .initialProperties(Properties.ITEM_COMBAT)
+    .register();*/
+
+    /*ItemEntry<ArmorItem> LEAD_BOOTS = builder("lead_boots")
+            .bootsItem(LEAD_ARMOR_MATERIAL)
+            .initialProperties(Properties.ITEM_COMBAT)
+    .register();*/
+
+    /*ItemEntry<HorseArmorItem> LEAD_HORSE_ARMOR = builder("lead_horse_armor")
+            .horseArmor(5, "lead")
+            .initialProperties(Properties.ITEM_COMBAT)
+    .register();*/
+
+    ItemEntry<Item> LEAD_INGOT = builder("lead_ingot")
             .tag(
                     AllTags.Items.Forge.INGOTS_LEAD,
                     AllTags.Items.Fabric.LEAD_INGOT, AllTags.Items.Fabric.LEAD_INGOTS
             )
     .register();
 
-    /*EnchancedTier LEAD_ITEM_TIER = TierBuilder.builder().level(2).uses(250).speed(6F).attackDamageBonus(2F).enchantmentValue(14).repairItem(AllItems.LEAD_INGOT).toolLevelTag(BlockTags.NEEDS_IRON_TOOL).build();
-    ArmorMaterial LEAD_ARMOR_MATERIAL = ArmorMaterialBuilder.builder(TestMod.ID, "lead").durabilityFromModifier(15).defenseFromSlotProtections(2, 5, 6, 2).enchantmentValue(9).toughness(0F).knockbackResistance(0F).repairItem(AllItems.LEAD_INGOT).build();
-
-    ItemRegistryEntry<BlockItem> LEAD_ORE = TestMod.Registries.ITEMS
-            .blockBuilder(AllBlocks.LEAD_ORE)
-            // .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
+    ItemEntry<Item> LEAD_NUGGET = builder("lead_nugget")
+            .tag(
+                    AllTags.Items.Forge.NUGGETS_LEAD,
+                    AllTags.Items.Fabric.LEAD_NUGGET, AllTags.Items.Fabric.LEAD_NUGGETS
+            )
     .register();
 
-    ItemRegistryEntry<BlockItem> DEEPSLATE_LEAD_ORE = TestMod.Registries.ITEMS
-            .blockBuilder(AllBlocks.DEEPSLATE_LEAD_ORE)
-            // .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
+    ItemEntry<Item> RAW_LEAD = builder("raw_lead")
+            .tag(AllTags.Items.Fabric.RAW_LEAD_ORE, AllTags.Items.Fabric.RAW_LEAD_ORES)
     .register();
-
-    ItemRegistryEntry<BlockItem> LEAD_BLOCK = TestMod.Registries.ITEMS
-            .blockBuilder(AllBlocks.LEAD_BLOCK)
-            // .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
-    .register();
-
-    ItemRegistryEntry<BlockItem> RAW_LEAD_BLOCK = TestMod.Registries.ITEMS
-            .blockBuilder(AllBlocks.RAW_LEAD_BLOCK)
-            // .tab(CreativeModeTab.TAB_BUILDING_BLOCKS)
-    .register();
-
-    ItemRegistryEntry<SwordItem> LEAD_SWORD = TestMod.Registries.ITEMS
-            .builders("lead_sword")
-            .sword(LEAD_ITEM_TIER)
-            .initialProperties(Properties.ITEM_COMBAT)
-    .register();
-
-    ItemRegistryEntry<PickaxeItem> LEAD_PICKAXE = TestMod.Registries.ITEMS
-            .builders("lead_pickaxe")
-            .pickaxeItem(LEAD_ITEM_TIER)
-            .initialProperties(Properties.ITEM_TOOL)
-    .register();
-
-    ItemRegistryEntry<AxeItem> LEAD_AXE = TestMod.Registries.ITEMS
-            .builders("lead_axe")
-            .axeItem(LEAD_ITEM_TIER)
-            .initialProperties(Properties.ITEM_TOOL)
-    .register();
-
-    ItemRegistryEntry<ShovelItem> LEAD_SHOVEL = TestMod.Registries.ITEMS
-            .builders("lead_shovel")
-            .shovelItem(LEAD_ITEM_TIER)
-            .initialProperties(Properties.ITEM_TOOL)
-    .register();
-
-    ItemRegistryEntry<HoeItem> LEAD_HOE = TestMod.Registries.ITEMS
-            .builders("lead_hoe")
-            .hoeItem(LEAD_ITEM_TIER)
-            .initialProperties(Properties.ITEM_TOOL)
-    .register();
-
-    ItemRegistryEntry<ArmorItem> LEAD_HELMET = TestMod.Registries.ITEMS
-            .builders("lead_helmet")
-            .helmetItem(LEAD_ARMOR_MATERIAL)
-            .initialProperties(Properties.ITEM_COMBAT)
-    .register();
-
-    ItemRegistryEntry<ArmorItem> LEAD_CHESTPLATE = TestMod.Registries.ITEMS
-            .builders("lead_chestplate")
-            .chestplateItem(LEAD_ARMOR_MATERIAL)
-            .initialProperties(Properties.ITEM_COMBAT)
-    .register();
-
-    ItemRegistryEntry<ArmorItem> LEAD_LEGGINGS = TestMod.Registries.ITEMS
-            .builders("lead_leggings")
-            .leggingItem(LEAD_ARMOR_MATERIAL)
-            .initialProperties(Properties.ITEM_COMBAT)
-    .register();
-
-    ItemRegistryEntry<ArmorItem> LEAD_BOOTS = TestMod.Registries.ITEMS
-            .builders("lead_boots")
-            .bootsItem(LEAD_ARMOR_MATERIAL)
-            .initialProperties(Properties.ITEM_COMBAT)
-    .register();
-
-    ItemRegistryEntry<HorseArmorItem> LEAD_HORSE_ARMOR = TestMod.Registries.ITEMS
-            .builders("lead_horse_armor")
-            .horseArmor(5, "lead")
-            .initialProperties(Properties.ITEM_COMBAT)
-    .register();
-
-    ItemRegistryEntry<Item> LEAD_INGOT = TestMod.Registries.ITEMS
-            .builders("lead_ingot")
-            .simpleItem()
-            .initialProperties(Properties.ITEM_GENERIC)
-    .register();
-
-    ItemRegistryEntry<Item> LEAD_NUGGET = TestMod.Registries.ITEMS
-            .builders("lead_nugget")
-            .simpleItem()
-            .initialProperties(Properties.ITEM_GENERIC)
-    .register();
-
-    ItemRegistryEntry<Item> RAW_LEAD = TestMod.Registries.ITEMS
-            .builders("raw_lead")
-            .simpleItem()
-            .initialProperties(Properties.ITEM_GENERIC)
-    .register();*/
 
     static void bootstrap()
     {
+    }
+
+    private static <T extends Item, P> ItemBuilder<T, P> builder(String name, P parent, ItemBuilder.ItemFactory<T> itemFactory)
+    {
+        return ItemBuilder.builder(TestMod.ID, name, parent, itemFactory);
+    }
+
+    private static <T extends Item> ItemBuilder<T, Object> builder(String name, ItemBuilder.ItemFactory<T> itemFactory)
+    {
+        return ItemBuilder.builder(TestMod.ID, name, itemFactory);
+    }
+
+    private static ItemBuilder<Item, Object> builder(String name)
+    {
+        return ItemBuilder.item(TestMod.ID, name);
+    }
+
+    private static <P> ItemBuilder<Item, P> builder(String name, P parent)
+    {
+        return ItemBuilder.item(TestMod.ID, name, parent);
     }
 }
