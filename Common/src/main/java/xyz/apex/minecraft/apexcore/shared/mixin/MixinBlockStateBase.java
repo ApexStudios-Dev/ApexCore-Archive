1371732254fcd297be31a3c883d9b31c5a631b1d
Apex <robsonlawson3@gmail.com>
2022-12-27 05:38:25 +0000

    Change to inject Hitbox mixin into BlockStateBase class

--- Common/src/main/java/xyz/apex/minecraft/apexcore/shared/mixin/MixinBlockStateBase.java
+++ Common/src/main/java/xyz/apex/minecraft/apexcore/shared/mixin/MixinBlockStateBase.java
@@ -1,12 +1,14 @@
 package xyz.apex.minecraft.apexcore.shared.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -14,18 +16,21 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 import xyz.apex.minecraft.apexcore.shared.registry.HitBoxRegistry;
 
-@Mixin(BlockBehaviour.class)
-public abstract class MixinBlockBehaviour
+@Mixin(BlockBehaviour.BlockStateBase.class)
+public abstract class MixinBlockStateBase
 {
+    @Shadow protected abstract BlockState asState();
+    @Shadow public abstract Block getBlock();
+
     @Inject(
-            method = "getShape",
+            method = "getShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;",
             at = @At("HEAD"),
             cancellable = true
     )
-    private void ApexCore$getShape(BlockState blockState, BlockGetter level, BlockPos pos, CollisionContext ctx, CallbackInfoReturnable<VoxelShape> cir)
+    private void ApexCore$getShape(BlockGetter level, BlockPos pos, CollisionContext ctx, CallbackInfoReturnable<VoxelShape> cir)
     {
-        HitBoxRegistry.findForBlock(blockState.getBlock()).ifPresent(entry -> {
-            var shape = entry.getShape(blockState);
+        HitBoxRegistry.findForBlock(getBlock()).ifPresent(entry -> {
+            var shape = entry.getShape(asState());
             cir.setReturnValue(shape);
             cir.cancel();
         });
