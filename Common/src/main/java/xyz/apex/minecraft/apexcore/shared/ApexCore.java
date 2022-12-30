db77dc6ca661564172a194ef10a2de14201d99ae
Apex <robsonlawson3@gmail.com>
2022-12-30 02:18:44 +0000

    Add common utility classes

--- Common/src/main/java/xyz/apex/minecraft/apexcore/shared/ApexCore.java
+++ Common/src/main/java/xyz/apex/minecraft/apexcore/shared/ApexCore.java
@@ -1,10 +1,13 @@
 package xyz.apex.minecraft.apexcore.shared;
 
+import xyz.apex.minecraft.apexcore.shared.util.Tags;
+
 public interface ApexCore
 {
     String ID = "apexcore";
 
     static void bootstrap()
     {
+        Tags.bootstrap();
     }
 }
---
+++ Common/src/main/java/xyz/apex/minecraft/apexcore/shared/util/function/package-info.java
@@ -0,0 +1,9 @@
+@FieldsAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package xyz.apex.minecraft.apexcore.shared.util.function;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
