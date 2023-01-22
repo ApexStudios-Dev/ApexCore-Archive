0e8f477c8baa80fb5eaccd43afeafad0888a3ff0
Apex <robsonlawson3@gmail.com>
2023-01-22 02:47:10 +0000

    Remove TestMod

--- TestMod-Fabric/src/datagen/java/xyz/apex/minecraft/testmod/fabric/package-info.java
+++
@@ -1,9 +0,0 @@
-@FieldsAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package xyz.apex.minecraft.testmod.fabric;
-
-import net.minecraft.FieldsAreNonnullByDefault;
-import net.minecraft.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
