7d7d5fdd90490b8d0611b4126eada71b4cc87546
Apex <robsonlawson3@gmail.com>
2023-03-18 10:51:56 +0000

    Add immovable tag Add helper class to register custom tags

--- common/src/main/java/xyz/apex/minecraft/apexcore/common/ApexCore.java
+++ common/src/main/java/xyz/apex/minecraft/apexcore/common/ApexCore.java
@@ -1,6 +1,15 @@
 package xyz.apex.minecraft.apexcore.common;
 
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
+import xyz.apex.minecraft.apexcore.common.util.TagHelper;
+
 public interface ApexCore
 {
     String ID = "apexcore";
+
+    interface BlockTags
+    {
+        TagKey<Block> IMMOVABLE = TagHelper.apexBlockTag("immovable");
+    }
 }
