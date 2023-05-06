119f6a1fe2589e3c4d6ee1a4a8ada8017bce827b
Apex <robsonlawson3@gmail.com>
2023-05-06 06:06:14 +0100

    Remove unnecessary SideOnly annotation from EntityHooks

--- common/src/main/java/xyz/apex/minecraft/apexcore/common/lib/hook/EntityHooks.java
+++ common/src/main/java/xyz/apex/minecraft/apexcore/common/lib/hook/EntityHooks.java
@@ -3,13 +3,10 @@ package xyz.apex.minecraft.apexcore.common.lib.hook;
 import net.minecraft.world.entity.Entity;
 import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.Nullable;
-import xyz.apex.minecraft.apexcore.common.lib.PhysicalSide;
-import xyz.apex.minecraft.apexcore.common.lib.SideOnly;
 
 /**
  * Hooks for various entity related things.
  */
-@SideOnly(PhysicalSide.CLIENT)
 @ApiStatus.NonExtendable
 public interface EntityHooks
 {
