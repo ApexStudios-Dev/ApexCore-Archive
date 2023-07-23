9f33637ed05c509ffd4abe08ff5cb531cf417ca3
Apex <robsonlawson3@gmail.com>
2023-07-23 01:31:15 +0100

    Add event invoked when entities fall on/collide with blocks.

--- platforms/common/src/main/java/xyz/apex/minecraft/apexcore/common/lib/event/types/EntityEvents.java
+++ platforms/common/src/main/java/xyz/apex/minecraft/apexcore/common/lib/event/types/EntityEvents.java
@@ -1,9 +1,11 @@
 package xyz.apex.minecraft.apexcore.common.lib.event.types;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.ApiStatus;
 import xyz.apex.minecraft.apexcore.common.lib.event.Event;
 import xyz.apex.minecraft.apexcore.common.lib.event.EventType;
@@ -15,6 +17,7 @@ public interface EntityEvents
     EventType<LeaveLevel> LEAVE_LEVEL = EventType.create(listeners -> (entity, level) -> listeners.forEach(listener -> listener.handle(entity, level)));
     EventType<LivingDeath> LIVING_DEATH = EventType.create(listeners -> (entity, source) -> listeners.stream().anyMatch(listener -> listener.handle(entity, source)));
     EventType<IsFakePlayer> IS_FAKE_PLAYER = EventType.create(listeners -> entity -> listeners.stream().anyMatch(listener -> listener.handle(entity)));
+    EventType<AfterEntityFallOn> AFTER_ENTITY_FALL_ON = EventType.create(listeners -> (entity, level, pos, blockState) -> listeners.forEach(listener -> listener.handle(entity, level, pos, blockState)));
 
     @ApiStatus.Internal
     static void bootstrap()
@@ -47,5 +50,12 @@ public interface EntityEvents
     interface IsFakePlayer extends Event
     {
         boolean handle(Entity entity);
+    }
+
+    @FunctionalInterface
+    @ApiStatus.NonExtendable
+    interface AfterEntityFallOn extends Event
+    {
+        void handle(Entity entity, Level level, BlockPos pos, BlockState blockState);
     }
 }
