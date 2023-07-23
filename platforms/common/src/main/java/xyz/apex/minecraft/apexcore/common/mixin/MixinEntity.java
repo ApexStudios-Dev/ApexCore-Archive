7f5bced7b8fa35344d79ba58d0edfdf07c2e89dc
Apex <robsonlawson3@gmail.com>
2023-07-23 18:20:58 +0100

    Fix Fabric crash during startup caused by AfterEntityFallOn Event Mixin

--- platforms/common/src/main/java/xyz/apex/minecraft/apexcore/common/mixin/MixinEntity.java
+++ platforms/common/src/main/java/xyz/apex/minecraft/apexcore/common/mixin/MixinEntity.java
@@ -4,34 +4,32 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import xyz.apex.minecraft.apexcore.common.lib.event.types.EntityEvents;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity
 {
     @Shadow private Level level;
+    @Shadow @Deprecated public abstract BlockPos getOnPosLegacy();
 
     @Inject(
             method = "move",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/world/level/block/Block;updateEntityAfterFallOn(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/world/entity/Entity;)V"
-            ),
-            locals = LocalCapture.CAPTURE_FAILHARD
+            )
     )
-    private void ApexCore$move(MoverType moverType, Vec3 pos, CallbackInfo ci, Vec3 collisionPos, double d, boolean bl, boolean bl2, BlockPos blockPos, BlockState blockState, Block block)
+    private void ApexCore$move(MoverType moverType, Vec3 pos, CallbackInfo ci)
     {
         var self = (Entity) (Object) this;
-
+        var blockPos = getOnPosLegacy();
+        var blockState = level.getBlockState(blockPos);
         EntityEvents.AFTER_ENTITY_FALL_ON.post().handle(self, level, blockPos, blockState);
     }
 }
