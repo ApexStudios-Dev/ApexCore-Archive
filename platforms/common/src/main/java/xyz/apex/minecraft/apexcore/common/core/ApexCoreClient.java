5cf883216d39f28750586dba97db25ae9fd200e9
Apex <robsonlawson3@gmail.com>
2023-08-06 22:57:05 +0100

    Add hooks to register reload listeners.

--- platforms/common/src/main/java/xyz/apex/minecraft/apexcore/common/core/ApexCoreClient.java
+++ platforms/common/src/main/java/xyz/apex/minecraft/apexcore/common/core/ApexCoreClient.java
@@ -7,6 +7,7 @@ import xyz.apex.minecraft.apexcore.common.core.client.BlockPlacementRenderer;
 import xyz.apex.minecraft.apexcore.common.lib.PhysicalSide;
 import xyz.apex.minecraft.apexcore.common.lib.SideOnly;
 import xyz.apex.minecraft.apexcore.common.lib.hook.ColorHandlerHooks;
+import xyz.apex.minecraft.apexcore.common.lib.hook.GenericHooks;
 import xyz.apex.minecraft.apexcore.common.lib.hook.RendererHooks;
 
 @ApiStatus.Internal
@@ -18,6 +19,7 @@ public interface ApexCoreClient
 
     ColorHandlerHooks COLOR_HANDLER_HOOKS = Services.singleton(ColorHandlerHooks.class);
     RendererHooks RENDERER_HOOKS = Services.singleton(RendererHooks.class);
+    GenericHooks CLIENT_HOOKS = Services.singleton(GenericHooks.class);
 
     @MustBeInvokedByOverriders
     default void bootstrap()
---
+++ platforms/neoforge/src/main/java/xyz/apex/minecraft/apexcore/neoforge/lib/hook/GenericHooksImpl.java
@@ -0,0 +1,26 @@
+package xyz.apex.minecraft.apexcore.neoforge.lib.hook;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.PreparableReloadListener;
+import net.minecraftforge.client.event.RegisterClientReloadListenersEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AddReloadListenerEvent;
+import org.jetbrains.annotations.ApiStatus;
+import xyz.apex.minecraft.apexcore.common.lib.PhysicalSide;
+import xyz.apex.minecraft.apexcore.common.lib.hook.GenericHooks;
+import xyz.apex.minecraft.apexcore.neoforge.core.ModEvents;
+import xyz.apex.minecraft.apexcore.neoforge.lib.EventBusHelper;
+
+@ApiStatus.Internal
+public final class GenericHooksImpl implements GenericHooks
+{
+    @Override
+    public void registerReloadListener(PackType packType, ResourceLocation id, PreparableReloadListener reloadListener)
+    {
+        switch(packType) {
+            case CLIENT_RESOURCES -> PhysicalSide.CLIENT.runWhenOn(() -> () -> ModEvents.active().addListener(RegisterClientReloadListenersEvent.class, event -> event.registerReloadListener(reloadListener)));
+            case SERVER_DATA -> EventBusHelper.addListener(MinecraftForge.EVENT_BUS, AddReloadListenerEvent.class, event -> event.addListener(reloadListener));
+        }
+    }
+}
