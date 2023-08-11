213e44d1001946f0b2124a45bbc85c52d6182648
Apex <robsonlawson3@gmail.com>
2023-08-11 04:22:12 +0100

    Add methods to modify default and placement directions for HorizontalFacingBlockComponent

--- platforms/common/src/main/java/xyz/apex/minecraft/apexcore/common/lib/component/block/types/HorizontalFacingBlockComponent.java
+++ platforms/common/src/main/java/xyz/apex/minecraft/apexcore/common/lib/component/block/types/HorizontalFacingBlockComponent.java
@@ -9,25 +9,45 @@ import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import org.apache.commons.lang3.Validate;
 import xyz.apex.minecraft.apexcore.common.core.ApexCore;
 import xyz.apex.minecraft.apexcore.common.lib.component.block.BaseBlockComponent;
 import xyz.apex.minecraft.apexcore.common.lib.component.block.BlockComponentHolder;
 import xyz.apex.minecraft.apexcore.common.lib.component.block.BlockComponentType;
+
+import java.util.function.Function;
 
 public final class HorizontalFacingBlockComponent extends BaseBlockComponent
 {
     public static final BlockComponentType<HorizontalFacingBlockComponent> COMPONENT_TYPE = BlockComponentType.register(ApexCore.ID, "horizontal_facing", HorizontalFacingBlockComponent::new);
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
+    private Direction defaultFacing = Direction.NORTH;
+    private Function<BlockPlaceContext, Direction> placementDirection = context -> context.getHorizontalDirection().getOpposite();
+
     private HorizontalFacingBlockComponent(BlockComponentHolder componentHolder)
     {
         super(componentHolder);
     }
 
+    public HorizontalFacingBlockComponent withDefaultFacing(Direction defaultFacing)
+    {
+        Validate.isTrue(!isRegistered(), "Can only change default facing during registration");
+        this.defaultFacing = defaultFacing;
+        return this;
+    }
+
+    public HorizontalFacingBlockComponent withPlacementDirection(Function<BlockPlaceContext, Direction> placementDirection)
+    {
+        Validate.isTrue(!isRegistered(), "Can only change default facing during registration");
+        this.placementDirection = placementDirection;
+        return this;
+    }
+
     @Override
     public BlockState registerDefaultBlockState(BlockState defaultBlockState)
     {
-        return defaultBlockState.setValue(FACING, Direction.NORTH);
+        return defaultBlockState.setValue(FACING, defaultFacing);
     }
 
     @Override
@@ -39,7 +59,7 @@ public final class HorizontalFacingBlockComponent extends BaseBlockComponent
     @Override
     public BlockState getStateForPlacement(BlockState placementBlockState, BlockPlaceContext context)
     {
-        return placementBlockState.setValue(FACING, context.getHorizontalDirection());
+        return placementBlockState.setValue(FACING, placementDirection.apply(context));
     }
 
     @Override
