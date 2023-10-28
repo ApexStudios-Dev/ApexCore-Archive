040341b8da0d2fd02dae591f605b97171945d3f5
Apex <robsonlawson3@gmail.com>
2023-10-28 05:02:37 +0100

    Convert entire project to use Architectury loom.
    Architectury is in compile only mode and set to not inject arch-injectables

--- testmod/mcforge/src/main/java/xyz/apex/minecraft/testmod/mcforge/TestModClientImpl.java
+++
@@ -1,9 +0,0 @@
-package xyz.apex.minecraft.testmod.mcforge;
-
-import org.jetbrains.annotations.ApiStatus;
-import xyz.apex.minecraft.testmod.common.TestModClient;
-
-@ApiStatus.Internal
-public final class TestModClientImpl implements TestModClient
-{
-}
