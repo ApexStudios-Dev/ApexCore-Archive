5e6dcacd81b7d8379d2d7ff99356ffd446cc29bb
Apex <robsonlawson3@gmail.com>
2023-12-06 05:24:25 +0000

    Update TestBlockWithEntity to keep track of its inventory state

--- testmod/common/src/main/java/dev/apexstudios/testmod/common/TestMod.java
+++ testmod/common/src/main/java/dev/apexstudios/testmod/common/TestMod.java
@@ -16,19 +16,19 @@ import dev.apexstudios.apexcore.common.registry.holder.DeferredBlockEntityType;
 import dev.apexstudios.apexcore.common.registry.holder.DeferredEntityType;
 import dev.apexstudios.apexcore.common.registry.holder.DeferredItem;
 import dev.apexstudios.apexcore.common.util.MenuHelper;
-import net.covers1624.quack.util.LazyValue;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.PigRenderer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.GlobalPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.*;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.ai.attributes.Attributes;
@@ -37,7 +37,6 @@ import net.minecraft.world.entity.animal.Pig;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.MenuConstructor;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
@@ -55,7 +54,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.function.Supplier;
+import java.util.function.Consumer;
 
 public interface TestMod
 {
@@ -97,8 +96,8 @@ public interface TestMod
             .defaultBlockEntity(TestBlockEntity::new)
     .register();
 
-    DeferredBlockEntityType<BlockEntity> TEST_BLOCK_ENTITY = DeferredBlockEntityType.createBlockEntityType(BLOCK_WITH_ENTITY.registryName());
-    DeferredHolder<MenuType<?>, MenuType<TestMenu>> TEST_MENU_TYPE = REGISTER.<TestMenu, BaseMenuScreen<TestMenu>>menu("test_menu", TestMenu::new, () -> () -> BaseMenuScreen::new);
+    DeferredBlockEntityType<TestBlockEntity> TEST_BLOCK_ENTITY = DeferredBlockEntityType.createBlockEntityType(BLOCK_WITH_ENTITY.registryName());
+    DeferredHolder<MenuType<?>, MenuType<TestMenu>> TEST_MENU_TYPE = REGISTER.<TestMenu, BaseMenuScreen<TestMenu>>menu("test_menu", TestMenu::forNetwork, () -> () -> BaseMenuScreen::new);
 
     GameRules.Key<GameRules.BooleanValue> TEST_GAMERULE = GameRules.register("test", GameRules.Category.MISC, GameRules.BooleanValue.create(false));
 
@@ -107,7 +106,7 @@ public interface TestMod
         REGISTER.register();
     }
 
-    final class BlockWithEntity extends BaseEntityBlock implements MenuConstructor
+    final class BlockWithEntity extends BaseEntityBlock
     {
         private int counter = 0;
 
@@ -116,7 +115,41 @@ public interface TestMod
             super(properties);
         }
 
-        @Nullable
+        @Override
+        public void setPlacedBy(Level level, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack)
+        {
+            if(stack.hasCustomHoverName())
+                TEST_BLOCK_ENTITY.getOptional(level, pos).ifPresent(blockEntity -> blockEntity.setCustomName(stack.getHoverName()));
+        }
+
+        @Override
+        public void onRemove(BlockState blockState, Level level, BlockPos pos, BlockState newBlockState, boolean movedByPiston)
+        {
+            if(!blockState.is(newBlockState.getBlock()))
+            {
+                TEST_BLOCK_ENTITY.getOptional(level, pos).ifPresent(blockEntity -> {
+                    blockEntity.oakInventory.forEach(stack -> Containers.dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), stack));
+                    blockEntity.birchInventory.forEach(stack -> Containers.dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), stack));
+                    level.updateNeighbourForOutputSignal(pos, this);
+                });
+
+                super.onRemove(blockState, level, pos, newBlockState, movedByPiston);
+            }
+        }
+
+        // TODO
+        /*@Override
+        public boolean hasAnalogOutputSignal(BlockState state)
+        {
+            return super.hasAnalogOutputSignal(state);
+        }
+
+        @Override
+        public int getAnalogOutputSignal(BlockState state, Level level, BlockPos pos)
+        {
+            return AbstractContainerMenu.getRedstoneSignalFromContainer();
+        }*/
+
         @Override
         public BlockEntity newBlockEntity(BlockPos pos, BlockState blockState)
         {
@@ -132,7 +165,9 @@ public interface TestMod
                 player.displayClientMessage(Component.literal("Server Counter: %s".formatted(count)), false);
                 TEST_PACKET_S2C.sendTo(count, server);
                 counter++;
-                MenuHelper.openMenu(server, Component.literal("Test Menu"), this, this::encodeExtraMenuData);
+
+                var blockEntity = TEST_BLOCK_ENTITY.getOptional(level, pos).orElseThrow();
+                MenuHelper.openMenu(server, blockEntity, blockEntity);
             }
 
             return InteractionResult.sidedSuccess(level.isClientSide);
@@ -142,56 +177,126 @@ public interface TestMod
         @Override
         public MenuProvider getMenuProvider(BlockState blockState, Level level, BlockPos pos)
         {
-            return MenuHelper.menuProvider(Component.literal("Test Menu"), this, this::encodeExtraMenuData);
-        }
-
-        @Nullable
-        @Override
-        public AbstractContainerMenu createMenu(int windowId, Inventory inventory, Player player)
-        {
-            return new TestMenu(windowId, inventory);
-        }
-
-        private void encodeExtraMenuData(FriendlyByteBuf buffer)
-        {
+            return TEST_BLOCK_ENTITY.getOptional(level, pos).map(blockEntity -> MenuHelper.wrapMenuProvider(blockEntity, blockEntity)).getRaw();
         }
     }
 
     final class TestMenu extends BaseMenu
     {
-        private TestMenu(int containerId, Inventory inventory, FriendlyByteBuf buffer)
-        {
-            this(containerId, inventory);
-        }
-
-        private TestMenu(int containerId, Inventory inventory)
+        private TestMenu(int containerId, Inventory inventory, ItemHandler oakItems, ItemHandler birchItems)
         {
             super(TEST_MENU_TYPE.value(), containerId, 176, 166);
 
             slotManager.addPlayerSlots(inventory);
-            slotManager.addSlot(new SimpleContainer(1), "oak", 20, 20, 0);
-            slotManager.addSlots(new SimpleContainer(4), "birch", 40, 20, 2, 2);
+            slotManager.addSlot(oakItems, "oak", 20, 20, 0);
+            slotManager.addSlots(birchItems, "birch", 40, 20, 2, 2);
             slotManager.addGhostSlot(80, 20).ghosting(Items.DIAMOND);
             slotManager.addGhostSlot(80, 40).ghosting(Items.NETHER_STAR);
             slotManager.addShiftTarget(SlotManager.GROUP_PLAYER, "oak", true);
             slotManager.addShiftTarget("birch", "oak", false);
         }
+
+        private static TestMenu forNetwork(int windowId, Inventory inventory, FriendlyByteBuf buffer)
+        {
+            var globalPos = buffer.readGlobalPos();
+            var dimension = globalPos.dimension();
+            var plrLevel = inventory.player.level();
+            var level = plrLevel.dimension() == dimension ? plrLevel : inventory.player.getServer().getLevel(globalPos.dimension());
+            var blockEntity = TEST_BLOCK_ENTITY.getOptional(level, globalPos.pos()).orElseThrow();
+            return new TestMenu(windowId, inventory, blockEntity.getItemHandler(), blockEntity.getItemHandler(Direction.NORTH));
+        }
     }
 
-    final class TestBlockEntity extends BlockEntity implements BlockEntityItemHandlerProvider
+    final class TestBlockEntity extends BlockEntity implements BlockEntityItemHandlerProvider, MenuProvider, Consumer<FriendlyByteBuf>, Nameable
     {
-        private final Supplier<ItemHandler> inventory = new LazyValue<>(() -> new SimpleItemHandler(1));
+        private static final String NBT_OAK = "Oak";
+        private static final String NBT_BIRCH = "Birch";
+        private static final String NBT_CUSTOM_NAME = "CustomName";
+
+        private ItemHandler oakInventory = new SimpleItemHandler(1);
+        private ItemHandler birchInventory = new SimpleItemHandler(4);
+        @Nullable private Component customName;
 
         private TestBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState blockState)
         {
             super(type, pos, blockState);
         }
 
-        @Nullable
+        @Override
+        public void load(CompoundTag tag)
+        {
+            if(tag.contains(NBT_OAK, Tag.TAG_COMPOUND))
+            {
+                var oakTag = tag.getCompound(NBT_OAK);
+                oakInventory = new SimpleItemHandler(1, oakTag);
+            }
+
+            if(tag.contains(NBT_BIRCH, Tag.TAG_COMPOUND))
+            {
+                var oakTag = tag.getCompound(NBT_BIRCH);
+                birchInventory = new SimpleItemHandler(4, oakTag);
+            }
+
+            if(tag.contains(NBT_CUSTOM_NAME, Tag.TAG_STRING))
+                customName = Component.Serializer.fromJsonLenient(tag.getString(NBT_CUSTOM_NAME));
+        }
+
+        @Override
+        protected void saveAdditional(CompoundTag tag)
+        {
+            tag.put(NBT_OAK, oakInventory.serialize());
+            tag.put(NBT_BIRCH, birchInventory.serialize());
+
+            if(customName != null)
+                tag.putString(NBT_CUSTOM_NAME, Component.Serializer.toJson(customName));
+        }
+
         @Override
         public ItemHandler getItemHandler(@Nullable Direction side)
         {
-            return inventory.get();
+            return side == null ? oakInventory : birchInventory;
+        }
+
+        @Override
+        public ItemHandler getItemHandler()
+        {
+            return oakInventory;
+        }
+
+        @Override
+        public AbstractContainerMenu createMenu(int windowId, Inventory inventory, Player player)
+        {
+            return new TestMenu(windowId, inventory, oakInventory, birchInventory);
+        }
+
+        @Override
+        public void accept(FriendlyByteBuf buffer)
+        {
+            buffer.writeGlobalPos(GlobalPos.of(level.dimension(), worldPosition));
+        }
+
+        public void setCustomName(Component customName)
+        {
+            this.customName = customName;
+        }
+
+        @Nullable
+        @Override
+        public Component getCustomName()
+        {
+            return customName;
+        }
+
+        @Override
+        public Component getName()
+        {
+            return getBlockState().getBlock().getName();
+        }
+
+        @Override
+        public Component getDisplayName()
+        {
+            return customName == null ? getName() : customName;
         }
     }
 }
