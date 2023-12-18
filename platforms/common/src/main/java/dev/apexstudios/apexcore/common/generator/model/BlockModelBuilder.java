e2ea7fdea2bf05b47adbd1413a51cd4d8a787ff8
Apex <robsonlawson3@gmail.com>
2023-12-18 18:11:41 +0000

    Add ability to specify render type with generated block models

--- platforms/common/src/main/java/dev/apexstudios/apexcore/common/generator/model/BlockModelBuilder.java
+++ platforms/common/src/main/java/dev/apexstudios/apexcore/common/generator/model/BlockModelBuilder.java
@@ -1,14 +1,17 @@
 package dev.apexstudios.apexcore.common.generator.model;
 
 import com.google.gson.JsonObject;
+import dev.apexstudios.apexcore.common.util.OptionalLike;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
+import org.jetbrains.annotations.Nullable;
 
 public final class BlockModelBuilder extends ModelBuilder<BlockModelBuilder, Block>
 {
     private static final boolean DEFAULT_AMBIENT_OCCLUSION = false;
 
     private boolean ambientOcclusion = DEFAULT_AMBIENT_OCCLUSION;
+    private OptionalLike<ResourceLocation> renderType = OptionalLike.empty();
 
     BlockModelBuilder(ModelGenerator<Block, BlockModelBuilder> generator, ResourceLocation path)
     {
@@ -26,11 +29,29 @@ public final class BlockModelBuilder extends ModelBuilder<BlockModelBuilder, Blo
         return ambientOcclusion(true);
     }
 
+    public BlockModelBuilder renderType(@Nullable ResourceLocation renderType)
+    {
+        this.renderType = OptionalLike.of(renderType);
+        return this;
+    }
+
+    public BlockModelBuilder renderType(String ownerId, String renderType)
+    {
+        return renderType(new ResourceLocation(ownerId, renderType));
+    }
+
+    public BlockModelBuilder renderType(String renderType)
+    {
+        return renderType(new ResourceLocation(renderType));
+    }
+
     @Override
     protected void serializeExtraData(JsonObject root)
     {
         if(ambientOcclusion != DEFAULT_AMBIENT_OCCLUSION)
             root.addProperty("ambientocclusion", ambientOcclusion);
+
+        renderType.map(ResourceLocation::toString).ifPresent(renderType -> root.addProperty("render_type", renderType));
     }
 }
 
